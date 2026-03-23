bb883aeee353ad43f5efb63fa72bd45a02b09054
ikar <ivankaruna@mail.ru>
2026-03-23 15:36:37 +0700

    Добавил тесты для страницы товаров

---
+++ pages/productsPage.ts
@@ -0,0 +1,41 @@
+import { Locator, Page } from '@playwright/test';
+
+export class ProductsPage {
+  readonly page: Page;
+  readonly productCart: Locator;
+  readonly cartLink: Locator;
+  readonly cartCount: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.productCart = page.locator('[data-test="inventory-item"]');
+    this.cartLink = page.locator('[data-test="shopping-cart-link"]');
+    this.cartCount = page.locator('[data-test="shopping-cart-link"]');
+  }
+
+  async navigate() {
+    await this.page.goto('/inventory.html');
+  }
+
+  async getProductByName(name: string) {
+    return this.page.locator('[data-test="inventory-item"]').filter({ hasText: name });
+  }
+
+  async addProductToCart(name: string) {
+    const product = await this.getProductByName(name);
+    await product.getByText('Add to cart').click();
+  }
+
+  async removeForomCart(name: string) {
+    const product = await this.getProductByName(name);
+    await product.getByText('Remove').click();
+  }
+
+  async getCartCount() {
+    return parseInt((await this.cartCount.textContent()) || '0');
+  }
+
+  async goToCart() {
+    await this.cartLink.click();
+  }
+}
---
+++ tests/products.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/loginPage';
+import { ProductsPage } from '../pages/productsPage';
+import { describe } from 'node:test';
+
+describe('Тесты продуктов', () => {
+  let productPage: ProductsPage;
+  const product = 'Sauce Labs Backpack';
+
+  test.beforeEach('Авторизация', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.login(process.env.USERNAME!, process.env.PASSWORD!);
+    productPage = new ProductsPage(page);
+    await expect(productPage.page).toHaveURL(/inventory/);
+  });
+  test('Отображение карточек товаров', async () => {
+    await expect(productPage.productCart).toHaveCount(6);
+  });
+
+  test('Добавление товара в корзину', async () => {
+    const initialCount = await productPage.getCartCount();
+    await productPage.addProductToCart(product);
+    await expect(productPage.cartCount).toHaveText((initialCount + 1).toString());
+  });
+
+  test('Удаление товара из корзины', async () => {
+    const initialCount = await productPage.getCartCount();
+    await productPage.addProductToCart(product);
+    await expect(productPage.cartCount).toHaveText((initialCount + 1).toString());
+    await productPage.removeForomCart(product);
+    expect(await productPage.getCartCount()).toEqual(initialCount);
+  });
+
+  test('Переход в корзину', async () => {
+    await productPage.addProductToCart(product);
+    await productPage.goToCart();
+    await expect(productPage.page).toHaveURL(/cart/);
+  });
+});
