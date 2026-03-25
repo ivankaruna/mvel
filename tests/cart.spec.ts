9b1fa840075c0da198196332e7f95c129d8e790b
ikar <ivankaruna@mail.ru>
2026-03-25 12:55:51 +0700

    Добавил тесты для страницы корзины

--- tests/cart.spec.ts
+++ tests/cart.spec.ts
@@ -4,17 +4,40 @@ import { LoginPage } from '../pages/loginPage';
 import { ProductsPage } from '../pages/productsPage';
 
 test.describe('Тесты корзины', () => {
-  let cartPage;
+  let cartPage: CartPage;
+  let productPage: ProductsPage;
+  const product1 = 'Sauce Labs Backpack';
+  const product2 = 'Sauce Labs Bike Light';
+
   test.beforeEach('Авторизация и добавление двух товаров', async ({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.navigate();
     await loginPage.login(process.env.USERNAME!, process.env.PASSWORD!);
-    const productPage = new ProductsPage(page);
-    await productPage.addProductToCart('Sauce Labs Backpack');
-    await productPage.addProductToCart('Sauce Labs Bike Light');
+
+    productPage = new ProductsPage(page);
+    await productPage.addProductToCart(product1);
+    await productPage.addProductToCart(product2);
     await productPage.goToCart();
+
     cartPage = new CartPage(page);
     await expect(cartPage.page).toHaveURL(/cart/);
   });
-  test('', async () => {});
+  test('Отображение добавленных товаров', async () => {
+    await expect(cartPage.productCart).toHaveCount(2);
+  });
+
+  test('Удаление товара из корзины', async () => {
+    await cartPage.removeProductFromCart(product1);
+    await expect(cartPage.productCart).toHaveCount(1);
+  });
+
+  test('Переход к списку товаров', async () => {
+    await cartPage.continueShopping();
+    await expect(cartPage.page).toHaveURL(/inventory/);
+  });
+
+  test('Переход на страницу подтверждения', async () => {
+    await cartPage.goToCheckout();
+    await expect(cartPage.page).toHaveURL(/checkout/);
+  });
 });
